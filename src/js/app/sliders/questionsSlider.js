693e33269b32f56fc4a72aa880c76350cd9acebb
Andrey Aratov <beg431@yandex.ru>
2020-03-31 10:54:41 +0300

    updated questions slider

--- src/js/app/sliders/questionsSlider.js
+++ src/js/app/sliders/questionsSlider.js
@@ -1,25 +1,37 @@
-const questionsSlider = new Swiper('.js-questions-slider', {
-	slidesPerView: 1,
-	spaceBetween: 0,
-	loop: true,
-	autoplay: {
-		delay: 5000,
-	},
-	effect: 'fade',
-	mousewheel: false,
-	fadeEffect: {
-		crossFade: true
-	},
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true
-	},
-	breakpoints: {
-		768: {
-			autoHeight: false
+const mobileBreakpoint = window.matchMedia('(min-width: 980px)');
+let questionsSlider;
+
+function initQuestionsSlider() {
+	questionsSlider = new Swiper('.js-questions-slider', {
+		slidesPerView: 1,
+		spaceBetween: 0,
+		loop: true,
+		autoplay: {
+			delay: 5000,
 		},
-		320: {
-			autoHeight: true
+		effect: 'fade',
+		mousewheel: false,
+		fadeEffect: {
+			crossFade: true
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
 		}
+	});
+}
+
+function checkBreakpoint() {
+	if (mobileBreakpoint.matches === false && typeof questionsSlider !== 'undefined') {
+		questionsSlider.destroy();
+	} else if (mobileBreakpoint.matches === true) {
+		initQuestionsSlider();
 	}
-});
+}
+
+const questionsSliderContainer = document.querySelector('.js-questions-slider');
+
+if (questionsSliderContainer) {
+	mobileBreakpoint.addListener(checkBreakpoint);
+	checkBreakpoint();
+}
